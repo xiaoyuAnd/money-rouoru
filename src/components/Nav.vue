f5b187d2a031a86d33b621d68b393064c3b3ea49
Frank <2521647197@qq.com>
2021-12-05 17:24:25 +0800

    Money页面布局

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,20 +1,25 @@
 <template>
   <div>
-     <div id>
-    <router-link to="/">记账</router-link> |
-    <router-link to="/Labels">标签</router-link>|
-    <router-link to="/Statistics">统计</router-link>
-  </div>
+    <div class="nav">
+      <router-link to="/">记账</router-link>
+      |
+      <router-link to="/Labels">标签</router-link>
+      |
+      <router-link to="/Statistics">统计</router-link>
+    </div>
   </div>
 </template>
 
 <script>
-  export default {
-    name:'nav'
-    
-  }
+export default {
+  name: 'nav'
+
+}
 </script>
 
 <style lang="scss" scoped>
+.nav{
+  border: 1px solid red;
+}
 
 </style>
--- src/views/Money.vue
+++ src/views/Money.vue
@@ -1,6 +1,8 @@
 <template>
-  <div>
-    Money
+  <div class="nav-wrapper">
+    <div class="content">
+      <p >Money</p>
+    </div>
     <Nav/>
   </div>
 </template>
@@ -16,5 +18,15 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+.nav-wrapper{
+  display: flex;
+  flex-direction: column;
+  height: 100vh;
+}
+
+.content{
+  flex-grow: 1;
+  overflow: auto;
+}
 
 </style>
