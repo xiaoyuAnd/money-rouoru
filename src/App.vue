092f56dbece01c3852c894feab19e8ef0b8f906a
Frank <2521647197@qq.com>
2021-12-07 22:31:34 +0800

    新建了style文件，设置了全局属性

--- src/App.vue
+++ src/App.vue
@@ -5,17 +5,15 @@
 </template>
 
 <style lang="scss">
-*{
-  padding: 0;
-  margin: 0;
-  box-sizing: border-box;
+@import "~@/assets/style/helper.scss";
+@import "~@/assets/style/reset.scss";
+body{
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #333;
+  font-family: $font-hei;
+  font-size: 16px;
+  line-height: 1.5;
 }
-//#app {
-//  font-family: Avenir, Helvetica, Arial, sans-serif;
-//  -webkit-font-smoothing: antialiased;
-//  -moz-osx-font-smoothing: grayscale;
-//  text-align: center;
-//  color: #2c3e50;
-//}
 
 </style>
