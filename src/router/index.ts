70fe2080623ee22f25387d568ddeef57e6b2f7fd
Frank <2521647197@qq.com>
2021-12-12 10:51:45 +0800

    新增EditLabel页面，使用$route及$router

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,45 +1,49 @@
 import Vue from 'vue'
-import VueRouter, { RouteConfig } from 'vue-router'
+import VueRouter, {RouteConfig} from 'vue-router'
 import Money from '@/views/Money.vue'
 import Labels from '@/views/Labels.vue'
 import Statistics from '@/views/Statistics.vue'
 import NotFound from '@/views/NotFound.vue'
-
+import EditLabel from '@/views/EditLabel.vue'
 Vue.use(VueRouter)
 
 const routes: Array<RouteConfig> = [
-  {
-    path:'/',
-    redirect:'/money'
-  },
-  {
-    path:'/money',
-    component:Money
-  },
-  {
-    path:'/labels',
-    component:Labels
-  },  {
-    path:'/statistics',
-    component:Statistics
-  },
-  {
-    path:'*',
-    component:NotFound
-  }
+    {
+        path: '/',
+        redirect: '/money'
+    },
+    {
+        path: '/money',
+        component: Money
+    },
+    {
+        path: '/labels',
+        component: Labels
+    }, {
+        path: '/statistics',
+        component: Statistics
+    },
+    {
+        path: '/labels/Edit/:id',
+        component: EditLabel
+    },
+    {
+        path: '*',
+        component: NotFound
+    }
 
-  // {
-  //   path: '/about',
-  //   name: 'About',
-  //   // route level code-splitting
-  //   // this generates a separate chunk (about.[hash].js) for this route
-  //   // which is lazy-loaded when the route is visited.
-  //   component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  // }
+    // {
+    //   path: '/about',
+    //   name: 'About',
+    //   // route level code-splitting
+    //   // this generates a separate chunk (about.[hash].js) for this route
+    //   // which is lazy-loaded when the route is visited.
+    //   component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    // }
 ]
 
 const router = new VueRouter({
-  routes
+    routes
 })
 
 export default router
---
+++ src/views/EditLabel.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <Layout>
+      11
+    </Layout>
+  </div>
+</template>
+
+<script>
+import {tagListModel} from "@/models/tagListModel";
+
+export default {
+  name: "EditLabel",
+  created() {
+    console.log(this.$route.params)
+    const id = this.$route.params.id
+    tagListModel.fetch()
+    const tags = tagListModel.data;
+    const tag = tags.filter(t => t.id === id)[0]
+    if(tag){
+      console.log(tag)
+    }else{
+      this.$router.replace('/404')
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
